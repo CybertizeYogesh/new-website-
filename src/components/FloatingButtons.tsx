f35c163d6cca0e98818dac8ae89afcbc283abbfc
CybertizeYogesh <cybertizeyogesh@gmail.com>
2026-07-20 17:37:19 +0530

    Position phone call floating button vertically above WhatsApp button on right side

--- src/components/FloatingButtons.tsx
+++ src/components/FloatingButtons.tsx
@@ -4,18 +4,18 @@ import React from 'react';
 
 export default function FloatingButtons() {
   return (
-    <>
-      <div className="floating-button-left">
+    <div className="floating-buttons-container">
+      <div className="floating-button-item call-item">
         <a 
           href="tel:+919876543210" 
           className="floating-btn call-btn" 
           aria-label="Call Us" 
           title="Call Us"
         >
-          <i className="fa-solid fa-phone col-bla"></i>
+          <i className="fa-solid fa-phone"></i>
         </a>
       </div>
-      <div className="floating-button-right">
+      <div className="floating-button-item whatsapp-item">
         <a 
           href="https://api.whatsapp.com/send?phone=919876543210&text=Hi%2C%20I%20need%20packers%20and%20movers%20services" 
           target="_blank" 
@@ -24,9 +24,9 @@ export default function FloatingButtons() {
           aria-label="Chat on WhatsApp" 
           title="Chat on WhatsApp"
         >
-          <i className="fa-brands fa-whatsapp col-bla"></i>
+          <i className="fa-brands fa-whatsapp"></i>
         </a>
       </div>
-    </>
+    </div>
   );
 }
